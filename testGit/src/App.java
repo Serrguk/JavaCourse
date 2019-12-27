74a5fcc8065c3831b2afa454e651e84749193805
serguk <serrguk@gmail.com>
2019-12-27 23:20:00 +0700

    Добавлен комментарий

--- testGit/src/App.java
+++ testGit/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.print(" world!");
+        System.out.print(" world!");    // Комментарий
     }
 }
