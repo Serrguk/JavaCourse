e6bcd5329e32626560124b4cda06786edb39f2d1
serguk <serrguk@gmail.com>
2019-12-27 23:21:57 +0700

    Еще че то добавили

--- testGit/src/App.java
+++ testGit/src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello!"); // 234
         System.out.print(" world!");    // Комментарий
+
+        // Чето
+        // Еще чето
     }
 }
